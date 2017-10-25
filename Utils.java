5f57c55136696da20f62ea09517e97df5696012f
minhnln <mingngo158@gmail.com>
2017-10-26 09:04:32 +1000

    create Utils.java for some useful user methods

---
+++ Utils.java
@@ -0,0 +1,16 @@
+
+public class Utils {
+	/*
+	 * positive modulo
+	 */
+	public static int positiveModulos(int a, int b) {
+		if (b == 0)
+			throw new ArithmeticException();
+		
+		if (b < 0) {
+			a = -a;
+			b = -b;
+		}
+		return (((a % b) + b) % b);
+	}
+}
