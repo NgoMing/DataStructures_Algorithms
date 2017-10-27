c52421e4cbdeae6f2a0758cfae16d35c03aee9ce
minhnln <mingngo158@gmail.com>
2017-10-28 07:22:21 +1000

    create ArrayQueue class

---
+++ DataStructure/ArrayQueue.java
@@ -0,0 +1,46 @@
+package DataStructure;
+
+import MyInterfaces.Queue;
+
+public class ArrayQueue implements Queue{
+	final static int CAPACITY = 1000;	// default value of capacity
+	
+	private int[] data;		// data array used for storage
+	private int frontIndex;	// index of the front element
+	private int size;		// current number of elements
+	
+	/*
+	 * Constructs queue with default capacity
+	 */
+	public ArrayQueue() { this(CAPACITY); }
+	
+	/*
+	 * Constructs queue with given capacity
+	 */
+	public ArrayQueue(int capacity) { data = new int[capacity]; }
+	
+	// Access methods
+	public int size() { return size; }
+	public boolean isEmpty() { return (size == 0); }
+	public int first() {
+		if (isEmpty()) return 0;
+		return data[frontIndex];
+	}
+	
+	// Update methods
+	public void enqueue (int element) throws IllegalStateException {
+		if (size == data.length) throw new IllegalStateException("Queue is full");
+		int availIndex = (frontIndex + size) % data.length;
+		data[availIndex] = element;
+		size ++;
+	}
+	
+	public int dequeue() {
+		if (isEmpty()) return 0;
+		int result = data[frontIndex];
+		data[frontIndex] = 0;
+		frontIndex = (frontIndex + 1) % data.length;
+		size --;
+		return result;
+	}
+}
