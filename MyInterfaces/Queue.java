3ed39071f8f532728d55fee3a72d41847679ae74
minhnln <mingngo158@gmail.com>
2017-10-27 23:03:10 +1000

    create Queue interface

---
+++ MyInterfaces/Queue.java
@@ -0,0 +1,28 @@
+package MyInterfaces;
+
+public interface Queue {
+	/*
+	 * Returns the number of the elements in the queue
+	 */
+	int size();
+	
+	/*
+	 * Tests whether the queue is empty
+	 */
+	boolean isEmpty();
+	
+	/*
+	 * Inserts an element at the rear of the queue
+	 */
+	void enqueue(int element);
+	
+	/*
+	 * Returns but not remove the first element of the queue (0 if empty)
+	 */
+	int first();
+	
+	/*
+	 * Removes and returns the first element of the queue (0 if empty)
+	 */
+	int dequeue();
+}
